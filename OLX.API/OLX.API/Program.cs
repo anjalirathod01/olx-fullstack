5875eff4105b00548e3617947f469f5aa93635dc
anjalirathod01 <ranjali0106@gmail.com>
2026-03-12 14:49:59 -1200

    updated product filter

--- OLX.API/OLX.API/Program.cs
+++ OLX.API/OLX.API/Program.cs
@@ -6,12 +6,12 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container
+        
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        // ✅ Add DbContext BEFORE builder.Build()
+        
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(
                 builder.Configuration.GetConnectionString("DefaultConnection")
--- olx/src/app/app.routes.ts
+++ olx/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
 
-// Since everything is in App component,
-// we use single route
 
 export const routes: Routes = [
   { path: '', component: App },
